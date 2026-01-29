85a347179d4cf70c258962df01550c2dd3405ec9
Vleena <vleenamascarenhas1998@gmail.com>
2026-01-29 15:23:12 +0530

    Even and Odd numbers

---
+++ Python/even_odd_list.py
@@ -0,0 +1,11 @@
+list = [1,2,3,4,5,6,7,8,9]
+odd = []
+even = []
+for n in list:
+    if n%2==0:
+        even.append(n)
+    else:
+        odd.append(n)
+
+print("Odd numbers:",odd)
+print("Even numbers:",even)
